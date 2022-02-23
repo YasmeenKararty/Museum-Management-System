4b42f811481cf3d17dc9e76de020dfb223af3345
Yasmeen ElKararty <66736704+YasmeenKararty@users.noreply.github.com>
2022-02-23 22:32:39 +0200

    Add files via upload

---
+++ Project/MainForm/MainForm/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace MainForm
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void connectedModeFormRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void buyATicketToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showExhibitsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            disconnectedModeForm disconnectedMode = new disconnectedModeForm();
+            disconnectedMode.Show();
+
+        }
+
+        private void visitorsToolStrip_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sectionDetailsReportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sectionDetailsReportForm sectiondetails = new sectionDetailsReportForm();
+            sectiondetails.Show();
+            
+
+        }
+
+        private void viewVistiorsDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            viewDetailsReportform viewDetails = new viewDetailsReportform();
+            viewDetails.Show();
+        }
+
+        private void buyATicketToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            connectedModeForm connectedMode = new connectedModeForm();
+            connectedMode.Show();
+        }
+    }
+}
